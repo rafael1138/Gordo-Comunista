03982e51f849b54734049645ed319adbdf7c15db
rafael1138 <rafael1138@protonmail.com>
2016-11-17 11:40:06 -0500

    Version 1.0
    Version 1.0 of the Firefox-Addon

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gordo-Comunista
+# A Firefox Extension for Find and Replace the text "Nicolas Maduro Moros" to "Gordo Comunista Feo".
+# The Original Idea was from Jesus Roldan Z, trying to make a funny way to read the "not so great" Venezuelan's news.
+# This is the Firefox Add-on Version.
+# Right now we're working in another plugin for make this like a little bigger, changing a lot of politicians names.
+
+#Versions:
+#1.0: Just a Firefox Add-on. You habe to Enable ir on the Firefox Browser's Add-on screen.
+
+#Code by @rafaelmmv
---
+++ gc.js
@@ -0,0 +1,42 @@
+﻿(function() {
+  walk(document.body);
+
+  function walk(node) {
+    // This wonderful function was made by T.J. Crowder
+    // Check it out here: http://stackoverflow.com/a/5904945
+    var child, next;
+
+    switch (node.nodeType) {
+      case 1:
+      case 9:
+      case 11:
+        child = node.firstChild;
+        while (child) {
+          next = child.nextSibling;
+          walk(child);
+          child = next;
+        }
+        break;
+      case 3:
+        maduroText(node);
+        break;
+    }
+  }
+  //Here is where the anti-comunist fairs do the job
+  function maduroText(textNode) {
+    var val = textNode.nodeValue;
+
+
+    val = val.replace(/Nicolás/g, "Gordo");
+    val = val.replace(/nicolás/g, "gordo");
+    val = val.replace(/Nicolas/g, "Gordo");
+    val = val.replace(/nicolas/g, "gordo");
+    val = val.replace(/Maduro/g, "Comunista");
+    val = val.replace(/maduro/g, "comunista");
+    val = val.replace(/Moros/g, "Feo");
+    val = val.replace(/moros/g, "feo");
+
+    textNode.nodeValue = val;
+  }
+
+})();
